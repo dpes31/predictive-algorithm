0f506aa53138613dcd563b194d38b0bfe947d88b
dpes31 <dpes31@gmail.com>
2026-06-30 18:43:12 +0900

    test(gate2): freeze approved 2.1.0 contract

--- tests/test_engine_contract.py
+++ tests/test_engine_contract.py
@@ -8,11 +8,12 @@ from engine.randomness_gate import GateState
 
 class EngineContractTests(unittest.TestCase):
     def test_frozen_versions_and_dimensions(self):
-        self.assertEqual(DEFAULT_CONFIG.model_version, "2.0.0-research")
-        self.assertEqual(DEFAULT_CONFIG.feature_contract_version, "1.0.0")
+        self.assertEqual(DEFAULT_CONFIG.model_version, "2.1.0-research")
+        self.assertEqual(DEFAULT_CONFIG.feature_contract_version, "1.1.0")
         self.assertEqual(DEFAULT_CONFIG.number_count, 45)
         self.assertEqual(DEFAULT_CONFIG.pick_count, 6)
         self.assertEqual(DEFAULT_CONFIG.candidate_count, 5)
+        self.assertEqual(DEFAULT_CONFIG.temperature_grid, (0.05, 0.10, 0.20, 0.50, 1.00))
 
     def test_gate_states_are_not_collapsed(self):
         self.assertEqual(
