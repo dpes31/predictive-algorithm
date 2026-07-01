fd7fe9307203f3aed47f3e3819c05b36baa14568
dpes31 <dpes31@gmail.com>
2026-07-01 10:51:20 +0900

    fix(r3): expose post-change expert

--- engine/experts/__init__.py
+++ engine/experts/__init__.py
@@ -3,6 +3,7 @@
 from .change_eprocess import ChangeEProcessDetector, ChangeEProcessResult
 from .persistence import build_persistence_model
 from .physical_evidence import PhysicalEvidenceModel, build_physical_evidence_model
+from .post_change import PostChangeModel, build_post_change_model
 from .regime_change import build_regime_change_model
 from .reversal import build_reversal_model
 from .uniform import build_uniform_model
@@ -12,6 +13,8 @@ __all__ = [
     "build_persistence_model",
     "build_reversal_model",
     "build_regime_change_model",
+    "build_post_change_model",
+    "PostChangeModel",
     "build_physical_evidence_model",
     "PhysicalEvidenceModel",
     "ChangeEProcessDetector",
