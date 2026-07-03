6962ed5b30730f604d5329422d2cd40f856f0df2
dpes31 <dpes31@gmail.com>
2026-07-03 21:27:15 +0900

    Wire Product Closeout C2 package exports

--- product_closeout/__init__.py
+++ product_closeout/__init__.py
@@ -1,5 +1,6 @@
 """Product Closeout Gate C2 offline QA package."""
 
-from .qa import QA_CONTRACT_VERSION, SPEC_CONTRACT_VERSION
+from .constants import QA_CONTRACT_VERSION, SPEC_CONTRACT_VERSION
+from .harness import run_internal_qa
 
-__all__ = ["QA_CONTRACT_VERSION", "SPEC_CONTRACT_VERSION"]
+__all__ = ["QA_CONTRACT_VERSION", "SPEC_CONTRACT_VERSION", "run_internal_qa"]
